e1d472a472b3af3fa378fc8fb178dbfbc086571d
Alex Ribes <alex.j.ribes@gmail.com>
2024-09-21 07:40:04 -0500

    Next.js Setup

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Poppins } from 'next/font/google';
+import "./globals.css";
+
+// If loading a variable font, you don't need to specify the font weight
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
