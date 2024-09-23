324f3ff67e4e5c97b26ca74932c5a03562e66e14
Alex Ribes <alex.j.ribes@gmail.com>
2024-09-23 16:42:35 -0500

    Styling updates to navbar and main page

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased`}
+        className={`${poppins.className} antialiased bg-green-100`}
       >
         <Navbar />
         {children}
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,31 @@
 import Link from "next/link";
 
+const LINKS = [
+    { src: "", title: "Explore"},
+    { src: "", title: "Upload"},
+    { src: "", title: "Messages"},
+];
+
 export default function Navbar() {
-    return <nav className="flex justify-between p-5">
-        <Link href={""}>Home</Link>
-        <Link href={""}>Explore</Link>
-        <Link href={""}>Upload</Link>
-        <Link href={""}>Messages</Link>
+    return <nav className="flex justify-between p-5 bg-teal-950 text-white">
+
+        <div>
+            <Link href={""}>Home</Link>
+        </div>
+
+        <div>
+            {LINKS.map(link => {
+                return <Link 
+                    href={link.src}
+                    className="ml-8 first:ml-0"
+                >
+                    {link.title}
+                </Link>
+            })}
+        </div>
+
+        <div>
+            <Link href={""}>Profile</Link>
+        </div>
     </nav>
 }
